b32e5ed862a739090973f21c8e9b2cb115d7769a
Cawa <cawa123@mail.ru>
2013-09-23 13:52:09 +0300

    yiic

--- protected/views/showcase/index.php
+++ protected/views/showcase/index.php
@@ -1,22 +1,50 @@
 <?php
 /* @var $this ShowcaseController */
 
-$this->breadcrumbs=array(
-	'Products',
+$this->breadcrumbs = array(
+    'Products',
 );
 ?>
-<?php 
+<?php
 $this->widget('zii.widgets.grid.CGridView', array(
     'dataProvider' => $products,
     'itemsCssClass' => 'table table-hover',
-    'htmlOptions' => array('class' => 'example')
+    'htmlOptions' => array('class' => 'example'),
     //'itemView' => '_view',
- //   'template' => "{items}\n{pager}",
+    //'template' => "{items}\n{pager}{edi",
+    'columns' => array(
+        'id',
+        'title',
+        'description',
+        'price',
+        'categorie_id',
+        'active',
+        'created_at',
+        array(
+            'class' => 'CButtonColumn',
+            'template' => '{edit}{delete}',
+            'buttons' => [
+                'edit' => [
+                    'label' => 'Edit', //Text label of the button.
+                    'options' => array('class' => 'btn btn-primary btn-xs'), //HTML options for the button tag.
+                    'visible' => 'true', //A PHP expression for determining whether the button is visible.
+                //'Yii::app()->createUrl("/customer/editmember1",array("id" => $data->primaryKey))',
+                ],
+                'delete' => [
+                    'label' => 'Delete', //Text label of the button.
+                    'options' => array('class' => 'btn btn-primary btn-xs'), //HTML options for the button tag.
+                    'visible' => 'true', //A PHP expression for determining whether the button is visible.
+                    'imageUrl'=> '',
+                //'Yii::app()->createUrl("/customer/editmember1",array("id" => $data->primaryKey))',
+                ],
+            ]
+        ),
+    ),
 ));
 ?>
 <h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
 
 <p>
-	You may change the content of this page by modifying
-	the file <tt><?php echo __FILE__; ?></tt>.
+    You may change the content of this page by modifying
+    the file <tt><?php echo __FILE__; ?></tt>.
 </p>
